40c576ff556d8ee2d92a397c52469cb08ef3dde2
Vadoo <example@example.com>
2023-10-25 11:09:29 +0330

    restyle  page

--- main.js
+++ main.js
@@ -1,42 +1,40 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var sidebar = document.getElementById("sidebar");
-    var sidebarWidth = sidebar.offsetWidth;
-    var toggleBtn = document.getElementById("toggle-btn");
-    var menuItems = document.querySelectorAll(".sidebar-list li");
-    var submenuItems = document.querySelectorAll(".submenu-toggle");
+  var sidebar = document.getElementById("sidebar");
+  var sidebarWidth = sidebar.offsetWidth;
+  var toggleBtn = document.getElementById("toggle-btn");
+  var menuItems = document.querySelectorAll(".sidebar-list li");
+  var submenuItems = document.querySelectorAll(".submenu-toggle");
 
-    // Toggle the sidebar when the toggle button is clicked
-    toggleBtn.addEventListener("click", function() {
+  toggleBtn.addEventListener("click", function() {
       sidebar.classList.toggle("show");
-    });
+      sidebarWidth = sidebar.offsetWidth; // بروزرسانی عرض سایدبار
+  });
 
-    // Add active state to the clicked menu item
-    menuItems.forEach(function(item) {
+  menuItems.forEach(function(item) {
       item.addEventListener("click", function() {
-        menuItems.forEach(function(item) {
-          item.classList.remove("active");
-        });
-        this.classList.add("active");
+          menuItems.forEach(function(item) {
+              item.classList.remove("active");
+          });
+          this.classList.add("active");
       });
-    });
+  });
 
-    // Toggle submenus
-    submenuItems.forEach(function(item) {
+  submenuItems.forEach(function(item) {
       item.addEventListener("click", function() {
-        this.classList.toggle("active");
-        var submenu = this.nextElementSibling;
-        submenu.classList.toggle("show");
+          this.classList.toggle("active");
+          var submenu = this.nextElementSibling;
+          submenu.classList.toggle("show");
       });
-    });
+  });
 
-    // Show/hide sidebar based on mouse position
-    document.addEventListener("mousemove", function(event) {
+  document.addEventListener("mousemove", function(event) {
       var mouseX = event.clientX;
 
       if (mouseX <= sidebarWidth && !sidebar.classList.contains("show")) {
-        sidebar.classList.add("show");
+          sidebar.classList.add("show");
+          sidebarWidth = sidebar.offsetWidth; // بروزرسانی عرض سایدبار
       } else if (mouseX > sidebarWidth && sidebar.classList.contains("show")) {
-        sidebar.classList.remove("show");
+          sidebar.classList.remove("show");
       }
-    });
   });
+});
